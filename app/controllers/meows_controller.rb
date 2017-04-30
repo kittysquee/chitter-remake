f2266ea3b379f6f19347d96baaef61a995e3efde
kittysquee <kittysquee@gmail.com>
2017-04-30 19:59:03 +0100

    Adds user class and Devise

--- app/controllers/meows_controller.rb
+++ app/controllers/meows_controller.rb
@@ -1,5 +1,6 @@
 class MeowsController < ApplicationController
   before_action :set_meow, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /meows
   # GET /meows.json
